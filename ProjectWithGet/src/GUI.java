141340af89ba904abeeeecb625e56e1c76b78d30
sayub <ayub.saad@ymail.com>
2013-01-28 12:12:55 -0500

    The GUI and the PDF things blahblah

---
+++ ProjectWithGet/src/GUI.java
@@ -0,0 +1,208 @@
+package com.java.pdfmaker;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Container;
+import java.awt.Desktop;
+import java.awt.Dimension;
+import java.awt.GraphicsEnvironment;
+import java.awt.GridLayout;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+import java.io.File;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JDesktopPane;
+import javax.swing.JFrame;
+import javax.swing.JInternalFrame;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JSlider;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+
+import org.jpedal.*;
+
+import com.itextpdf.text.pdf.BaseFont;
+
+
+
+public class GUI extends JFrame {
+	private JPanel rightPane;
+	private JPanel buttonPane;
+	private JPanel sliderPane;
+	private JButton open;
+	private JButton convertPDF;
+	private JButton convertASCII;
+	private JList<String> fontNames;
+	private JSlider spacing;
+	private JSlider font;
+	private JInternalFrame pane;
+	
+	String filePath = "C:/CSE2311/output.pdf";
+	String [] fonts = {BaseFont.HELVETICA, BaseFont.HELVETICA_OBLIQUE, BaseFont.HELVETICA_BOLD, BaseFont.HELVETICA_BOLDOBLIQUE,
+			BaseFont.TIMES_ROMAN, BaseFont.TIMES_ITALIC, BaseFont.TIMES_BOLD, BaseFont.TIMES_BOLDITALIC}; 
+	
+	public GUI(){
+		buttonPane = new JPanel(new GridLayout(7, 1));
+		open = new JButton("Open File");
+		convertPDF = new JButton("Convert to PDF");
+		convertASCII = new JButton("Convert to ASCII");
+		
+		buttonPane.add(new JLabel());
+		buttonPane.add(open);
+		buttonPane.add(new JLabel());
+		buttonPane.add(convertPDF);
+		buttonPane.add(new JLabel());
+		buttonPane.add(convertASCII);
+		buttonPane.add(new JLabel());
+		buttonPane.setBorder(BorderFactory.createLineBorder(Color.black));
+		
+		sliderPane = new JPanel(new GridLayout(3, 1, 0, 40));
+		
+		fontNames = new JList<String>(fonts);
+		//fontNames.setSelectedIndex(0);
+		fontNames.setBorder(BorderFactory.createTitledBorder("Select Font:"));
+		fontNames.addListSelectionListener(new ListSelectionListener() {
+			public void valueChanged(ListSelectionEvent e) {
+				Thread t = new Thread() {
+					public void run() {
+						try{
+							if(fontNames.getSelectedValue() != null)
+								new PdfMaker(filePath, fontNames.getSelectedValue().toString(), font.getValue(), spacing.getValue()).createPDF();
+							else
+								new PdfMaker(filePath, BaseFont.TIMES_ROMAN, font.getValue(), spacing.getValue()).createPDF();
+					
+							PdfRenderer();
+						}
+						catch(Exception ex) {
+							//ex.printStackTrace();
+						}
+					}
+				};
+				t.start();
+            }
+
+			
+		});
+		
+		font = new JSlider(1, 10);
+		font.setBorder(BorderFactory.createTitledBorder("Font Size:"));
+		font.setMajorTickSpacing(5);
+		font.setMinorTickSpacing(1);
+		font.setPaintTicks(true);
+		font.addChangeListener(new ChangeListener() {
+			public void stateChanged (ChangeEvent e) {
+				Thread t = new Thread() {
+					public void run() {
+				
+						try{
+							if(fontNames.getSelectedValue() != null)
+								new PdfMaker(filePath, fontNames.getSelectedValue().toString(), font.getValue(), spacing.getValue()).createPDF();
+							else
+								new PdfMaker(filePath, BaseFont.TIMES_ROMAN, font.getValue(), spacing.getValue()).createPDF();
+							PdfRenderer();
+						}
+						catch(Exception ex) {
+							//ex.printStackTrace();
+						}
+					}
+				};
+				t.start();
+				
+			}
+		});
+		
+		spacing = new JSlider(1, 10);
+		spacing.setBorder(BorderFactory.createTitledBorder("Spacing:"));
+		spacing.setMajorTickSpacing(5);
+		spacing.setMinorTickSpacing(1);
+		spacing.setPaintTicks(true);
+		spacing.addChangeListener(new ChangeListener() {
+			public void stateChanged (ChangeEvent e) {
+				Thread t = new Thread() {
+					public void run() {
+				
+						try{
+							if(fontNames.getSelectedValue() != null)
+								new PdfMaker(filePath, fontNames.getSelectedValue().toString(), font.getValue(), spacing.getValue()).createPDF();
+							else
+								new PdfMaker(filePath, BaseFont.TIMES_ROMAN, font.getValue(), spacing.getValue()).createPDF();
+					
+							PdfRenderer();
+						}
+						catch(Exception ex) {
+							//ex.printStackTrace();
+						}
+					}
+				};
+				t.start();
+				
+			}
+		});
+		
+		
+		//sliderPane.add(new JLabel());
+		sliderPane.add(font);
+		//sliderPane.add(new JLabel());
+		sliderPane.add(spacing);
+		
+		//sliderPane.add(new JLabel());
+		sliderPane.setBorder(BorderFactory.createLineBorder(Color.black));
+		
+		
+		rightPane = new JPanel(new GridLayout(3, 1));
+		rightPane.add(buttonPane);
+		rightPane.add(new JScrollPane(fontNames), BorderLayout.CENTER);
+		rightPane.add(sliderPane);
+		rightPane.setBorder(BorderFactory.createLineBorder(Color.black));
+		
+		Container c = getContentPane();
+		c.setLayout(new BorderLayout());
+		c.add(rightPane, BorderLayout.EAST);
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		setSize(screenSize);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setVisible(true);
+	}
+	
+	public void PdfRenderer() {
+		/*try{
+			PdfMaker pdf = new PdfMaker(filePath, fontNames.getSelectedItem().toString(), font.getValue(), spacing.getValue());
+			//System.out.println(spacing.getValue());
+			pdf.createPDF();
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}*/
+		
+		PdfDecoder pdfView = new PdfDecoder();
+		
+		
+		try {
+			pdfView.openPdfFile(filePath);
+			pdfView.decodePage(1);
+			pdfView.setPageParameters(2, 1);
+		} 
+		catch (Exception e) {
+			//e.printStackTrace();
+		}
+		getContentPane().add(pdfView, BorderLayout.CENTER);
+		validate();
+	}
+	
+	public static void main (String args[]) {
+		GUI gui = new GUI();
+		gui.PdfRenderer();
+	}
+}
---
+++ ProjectWithGet/src/PdfMaker.java
@@ -0,0 +1,263 @@
+package com.java.pdfmaker;
+
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.pdf.*;
+
+public class PdfMaker {
+	String filePath = null;
+	Document document;
+	PdfWriter writer;
+	PdfContentByte cb;
+	BaseFont bF;
+	Dimension screenSize;
+	double width;
+	double height;
+	String fontName = "";
+	float fontSize = 8f;
+	float spacing;
+	
+	String s = 
+			"|-------------------------|-------------------------|\n" +
+			"|-----1-----1-----1-----1-|-----1-----1-----1-----1-|\n" +
+			"|---2-----2-----2-----2---|---2-----2-----2-----2---|\n" +
+			"|-2-----2-----2-----2-----|-2-----2-----2-----2-----|\n" +
+			"|-0-----------------------|-------------------------|\n" +
+			"|-------------------------|-3-----------------------|";
+	
+	String s1 = "|-------------------------|-------------------------|\n" +
+				"|-----1-----1-----3-----3-|-----3-----1-----0-----0-|\n" +
+				"|---2-----2-----3-----3---|---1-----2-----2-----1---|\n" +
+				"|-3-----3-----3-----3-----|-2-----2-----2-----0-----|\n" +
+				"|-------------5-----------|-------------------------|\n" +
+				"|-1-----------------------|-0-----------0-----------|";
+	
+	String s2 = "|-------------------0----0|-0-----------------0----0|\n" +
+				"|-----------1-----1-----1-|-----3-----3-----3-----3-|\n" +
+				"|-2---2---2-----2-----2---|---4-----4-----4-----4---|\n" +
+				"|-2-2---2-----2-----2-----|-------2-----2-----2-----|\n" +
+				"|-0-----------------------|-------------------------|\n" +
+				"|-------------------------|-0-----------------------|";
+	
+	String s3 = "|-------------------0----0|-0-----------------0----0|\n" +
+				"|-----------1-----1-----1-|-----3-----3-----3-----3-|\n" +
+				"|-3---6---9-----3-----3---|---4-----4-----4-----4---|\n" +
+				"|-2-3---1-----1-----4-----|-------2-----2-----2-----|\n" +
+				"|-1-----------------------|-------------------------|\n" +
+				"|-------------------------|-0-----------------------|";
+	
+	String s4 = "|------- ---- --------------|------------------ ------ -|\n" +
+				"|-1---1- ---1 1-----1-----1-|------------------ ------ -|\n" +
+				"|---0--- --0- ----0-----0---|-----5-----5-----5 8----5 8|\n" +
+				"|-2----2 2--- --2-----2-----|---5-----5-----5-- --5--- -|\n" +
+				"|-3----- ---- --------------|-6-----6-----6---- 6----- -|\n" +
+				"|------- ---- ---3----------|-4---------------- 4----- -|";
+	
+	String s5 = "|-------------------0----0|-0-----------------0----0|\n" +
+				"|-----------1-----1-----1-|-----4-----7-----2-----3-|\n" +
+				"|-8---8---9-----8-----0---|---4-----4-----4-----4---|\n" +
+				"|-2-3---1-----1-----4-----|-------2-----2-----2-----|\n" +
+				"|-1-----------------------|-------------------------|\n" +
+				"|-------------------------|-0-----------------------|";
+	
+	
+	
+	ArrayList<String> contents = new ArrayList<String>();
+	
+	//public static void main(String[] args) throws DocumentException, IOException{
+		//FirstPDF pdf = new FirstPDF();
+		//pdf.createPDF();
+//	}
+	
+	public PdfMaker(String filePath, String fontName, float fontSize, float spacing) throws DocumentException, IOException {
+		this.filePath = filePath;
+		this.fontName = fontName;
+		this.spacing = spacing;
+		this.fontSize = fontSize;
+		//Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		//width = screenSize.getWidth();
+		//height = screenSize.getHeight();
+		
+		document = new Document();
+		writer = PdfWriter.getInstance(document, new FileOutputStream(filePath));
+		document.open();
+		
+		contents.add(s);
+		contents.add(s1);
+		contents.add(s2);
+		contents.add(s3);
+		contents.add(s4);
+		contents.add(s5);
+	}
+	
+	public void createPDF() throws DocumentException, IOException {
+		cb = writer.getDirectContent();
+		bF = BaseFont.createFont(fontName,
+		          BaseFont.WINANSI, BaseFont.EMBEDDED);
+		cb.setFontAndSize(bF, 20f);
+		cb.setLineWidth(0.5f);
+	    cb.beginText();
+		cb.showTextAligned(PdfContentByte.ALIGN_CENTER, "Moonlight Sonata", 310, document.top(), 0);	
+		cb.setFontAndSize(bF, 10f);
+		cb.showTextAligned(PdfContentByte.ALIGN_CENTER, "Ludwig van Beethoven", 315, document.top() - 15, 0);
+		cb.endText();
+		cb.stroke();
+		cb.setFontAndSize(bF, fontSize);
+		
+		if(bF.getWidthPoint('3', fontSize) > spacing) {
+			spacing = bF.getWidthPoint('3', fontSize);
+		}
+		
+		float yIncrement = 40f;
+		float yPos;
+		
+		
+		float xPos = document.left();
+		float margin = document.left();
+		yPos = document.top() - yIncrement;
+		
+		for(int no = 0; no < contents.size();) {
+			for(int i = 0, line = 1; i < contents.get(no).length(); i++) {
+				if(contents.get(no).charAt(i) == '-'){
+					
+					cb.moveTo(xPos, yPos);
+			
+					float newXPos = xPos + spacing;
+					cb.lineTo(newXPos, yPos);
+					
+					xPos = newXPos;
+				}
+				
+				else if(Character.isDigit(contents.get(no).charAt(i))){
+					cb.beginText();
+					cb.showTextAlignedKerned(PdfContentByte.ALIGN_LEFT, Character.toString(contents.get(no).charAt(i)), xPos, yPos- fontSize/2f, 0);	
+					cb.endText();
+					xPos += spacing;
+				}
+			
+				else if(contents.get(no).charAt(i) == '|'){
+					if(line < 6) {
+						cb.moveTo(xPos, yPos);
+						cb.lineTo(xPos, yPos - fontSize);
+					}
+				}
+				
+				cb.stroke();
+			
+				if(contents.get(no).charAt(i) == '\n'){
+					xPos = margin;
+					yPos -= fontSize;
+					line++;
+				}	
+			}
+			
+			no++;
+			
+			if(no >= contents.size()) {
+				break;
+			}
+			
+			if(xPos + ((contents.get(no).substring(0, contents.get(no).indexOf('\n') - 1).length()) * spacing) < document.right()) {
+				//float fl = ((contents.get(no + 1).substring(0, contents.get(no + 1).indexOf('\n') - 1).length()) * spacing) < document.right();
+				//System.out.println("fl: " + fl + ", " + "left: ")
+				margin = xPos;
+				yPos = document.top() - yIncrement;
+			}
+			else {
+				xPos = document.left();
+				yIncrement += (fontSize * 8);
+				yPos = document.top() - yIncrement;
+				margin = document.left();
+			}
+				
+
+		
+			/*for(int i = 0, line = 1; i < contents.get(no).length(); i++) {
+			
+				if(contents.get(no).charAt(i) == '-'){
+					
+					cb.moveTo(xPos, yPos);
+			
+					float newXPos = xPos + spacing;
+					cb.lineTo(newXPos, yPos);
+					
+					xPos = newXPos;
+				}
+				
+				else if(Character.isDigit(contents.get(no).charAt(i))){
+					cb.beginText();
+					cb.showTextAlignedKerned(PdfContentByte.ALIGN_LEFT, Character.toString(contents.get(no).charAt(i)), xPos, yPos- fontSize/2f, 0);	
+					cb.endText();
+					xPos += spacing;
+				}
+			
+				else if(contents.get(no).charAt(i) == '|'){
+					if(line < 6) {
+						cb.moveTo(xPos, yPos);
+						cb.lineTo(xPos, yPos - fontSize);
+					}
+				}
+				
+				cb.stroke();
+			
+				if(contents.get(no).charAt(i) == '\n'){
+					xPos = document.left();
+					yPos -= fontSize;
+					line++;
+				}	
+			}
+		
+			no++;
+
+			if(no >= contents.size()) {
+				break;
+			}
+			float newLeft = xPos;
+			yPos = document.top() - yIncrement;
+			
+			for(int i = 0, line = 1; i < contents.get(no).length(); i++) {
+			
+				if(contents.get(no).charAt(i) == '-'){
+					cb.moveTo(xPos, yPos);
+					float newXPos = xPos + spacing;
+					cb.lineTo(newXPos, yPos);
+					xPos = newXPos;
+				}
+				
+				else if(Character.isDigit(contents.get(no).charAt(i))){
+					cb.beginText();
+					cb.showTextAlignedKerned(PdfContentByte.ALIGN_LEFT, Character.toString(contents.get(no).charAt(i)), xPos, yPos - fontSize/2f, 0);	
+					cb.endText();
+					xPos += spacing;
+				}
+			
+				else if(contents.get(no).charAt(i) == '|'){
+					if(line < 6) {
+						cb.moveTo(xPos, yPos);
+						cb.lineTo(xPos, yPos - fontSize);
+					}
+				}
+				
+				cb.stroke();
+			
+				if(contents.get(no).charAt(i) == '\n'){
+				
+					xPos = newLeft;
+					yPos -= fontSize;
+					line++;
+				}	
+			}
+			
+			yIncrement += (fontSize * 8);*/
+		}
+		
+		document.close();
+	}
+
+}
