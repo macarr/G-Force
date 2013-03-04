8b057bf08e2ae2741da5a06822f16e1f0d397293
joychoi <joychoi@yorku.ca>
2013-03-04 12:45:25 -0500

    Changed PdfMaker so that it takes in just the inputParser as a parameter. Also added test cases and JUnit tester

---
+++ ProjectWithGet/Tests/com/java/pdfmaker/InputConverter.java
@@ -0,0 +1,117 @@
+package com.java.pdfmaker;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import com.itextpdf.text.DocumentException;
+
+class inputConverter {
+
+	String title = "";
+	String subtitle = "";
+	ArrayList<ArrayList<String>> contents;
+	
+	public inputConverter(String inputPath)
+	{
+		
+		ArrayList<String> block = new ArrayList<String>();
+		this.contents = new ArrayList<ArrayList<String>>();
+		Boolean Btitle = true;
+		Boolean Bsubtitle = true;
+		
+		try{
+		
+			//Input file
+			BufferedReader in = new BufferedReader(new FileReader(inputPath));
+			
+			//Get rid of all preceding empty lines
+			String current = in.readLine();
+			while(current.equals(""))
+				current = in.readLine();
+					
+			//Reading in the title.
+			if(current.toLowerCase().startsWith("title"))
+				this.title = current.substring(6);
+			else if(current.charAt(0) != '|' && current.charAt(0) != '-' )
+				this.title = current;
+			else{
+				this.title = "Not given";
+				Btitle = false;
+				Bsubtitle = false;
+			}
+					
+			//Get rid of all empty lines, provided a title was provided
+			if(Btitle == true)
+			{
+				current = in.readLine();
+				while(current.equals(""))
+					current = in.readLine();
+			}
+					
+			//Reading in the subtitle
+			if(current.toLowerCase().startsWith("subtitle"))
+				this.subtitle = current.substring(9);
+			else if(current.charAt(0) != '|' && current.charAt(0) != '-' )
+				this.subtitle = current;
+			else{
+				this.subtitle = "Not given";
+				Bsubtitle = false;
+			}
+			
+			//Get rid of all empty lines, provided a subtitle was provided
+			if(Bsubtitle == true)
+			{
+				current = in.readLine();
+				while(current != null && (current.equals("") || current.charAt(0) != '|' ))
+					current = in.readLine();
+			}
+			
+			
+			int line = 1;
+	 	
+			while(current != null) {
+				//Any empty lines and garbage lines (i.e. lines that don't start with '|' are passed over
+				if(current.equals("") || current.charAt(0) != '|');
+				else if (line > 6)
+				{
+					this.contents.add(block);
+					block = new ArrayList<String>();
+		 			line = 2;
+		 			block.add(current);
+				}
+				else{
+		 			block.add(current);
+		 			line++;
+				}
+				current = in.readLine();
+			}
+			
+			if(!block.isEmpty())
+				this.contents.add(block);
+			
+			in.close();
+		}
+		
+		catch(Exception e){
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public String getTitle()
+	{
+		return this.title;
+	}
+	
+	public String getSubtitle()
+	{
+		return this.subtitle;
+	}
+	
+	public ArrayList<ArrayList<String>> getData()
+	{
+		return this.contents;
+	}
+}
--- ProjectWithGet/src/com/java/pdfmaker/PdfMaker.java
+++ ProjectWithGet/src/com/java/pdfmaker/PdfMaker.java
@@ -10,53 +10,55 @@ public class PdfMaker {
   private Document document;
 	private PdfWriter writer;
 	private ArrayList<ArrayList<String>> contents;
-	
+	private inputParser in;
+
 	private float fontSize;
 	private float spacing;
-	
+
 	private ArrayList<DIndexFreq> dIndexes;
 	private int curDIndex = 0;
-	
+
 	private float yIncrement = 40f;
 	private float margin;
 	private float xPos;
 	private float yPos;
-	
+
 	private int blockNum;
 	private int lineNum;
 	private int charNum = 0;
 	private String line = "";
-	
+
 	private int skip = 0;
-	
+
 	private float []lastNumPos = new float[6];
-	
+
 	private TabUtilitiesBundle tUB;
-	
-	public PdfMaker(ArrayList<String> header, ArrayList<ArrayList<String>> contents, Rectangle pageSize, String filePath, String fontName, float fontSize, float spacing) {
-		this.contents = contents;
+
+	public PdfMaker(inputParser in, Rectangle pageSize, String filePath, String fontName, float fontSize, float spacing) {
+		this.in = in;
+		this.contents = in.getData();
 		this.spacing = spacing;
 		this.fontSize = fontSize;
-		
+
 		document = new Document(pageSize);
-		
+
 		try{
 			writer = PdfWriter.getInstance(document, new FileOutputStream(filePath));
 		}
 		catch(Exception e){
 			e.printStackTrace();
 		}
-		
+
 		document.open();
-		
+
 		tUB = new TabUtilitiesBundle(writer.getDirectContent(), fontName, fontSize, spacing, pageSize);
 		tUB.setLineWidth(0.5f);
-		tUB.processHeader(header, document.getPageSize().getWidth(), document.top());
+		tUB.processHeader(in, document.getPageSize().getWidth(), document.top());
 	}
-	
+
 	public boolean createPDF() {
 		boolean fullPdfWritten = true;
-		
+
 		margin = document.left();
 		xPos = margin;
 		yPos = document.top() - yIncrement;
@@ -69,14 +71,14 @@ public class PdfMaker {
 			//at the beginning of each new block the y-position and x-position need to be set
 			yPos = document.top() - yIncrement;
 			xPos = margin;
-	
+
 			if(dIndexes.get(curDIndex).spaceNeeds > document.right()-document.left()){
 				fullPdfWritten = false;
 			}
 			//calling this method to draw the beginning 6 horizontal lines
 			tUB.processBeginningHorizontalLines(contents.get(blockNum).size(), document.left(), yPos);
 			yPos = document.top()-yIncrement;
-			
+
 			//skip > 0 means that the block was split and the trailing '|'s were printed before the split,
 			//and suppressed on the current position. Instead, the single '|' would be printed, which is done
 			//through maintaining the skip variable and a call to drawSingleBars.
@@ -84,22 +86,22 @@ public class PdfMaker {
 				tUB.drawSingleBars(contents.get(blockNum).size(), xPos, yPos);
 				yPos = document.top() - yIncrement;
 			}
-			
+
 			//yPos = document.top() - yIncrement;
 			//at the beginning of each block the xPos needs to be set to the margin.
 			for(lineNum = 0; lineNum < contents.get(blockNum).size(); lineNum++){
 				line = contents.get(blockNum).get(lineNum);
-		
+
 				xPos = margin;
-				
+
 				processUnitLine();
 				//after printing the parts of each line, we need to move to the next 
 				yPos -= fontSize;
 			}
-			
+
 			curDIndex++;
 			skip = 0;
-			
+
 			//if current character index is less than the index of the first of the last set of '|'s
 			if(curDIndex < dIndexes.size()-1){
 				processMidBlockReInit();
@@ -109,10 +111,10 @@ public class PdfMaker {
 			else{
 				processEndBlockReInit();
 			}
-			
+
 			if(blockNum < contents.size())
 				dIndexes = getDIndexes(contents.get(blockNum).get(1));
-			
+
 			//if there is no more space left to join the next block right after the current block, set the margin
 			//to the left end of the document, and yIncrement is increased to set the gap between the current block
 			//and the next.
@@ -122,12 +124,12 @@ public class PdfMaker {
 				processInsufficientSpaceReInit();
 			}
 		}
-		
+
 		document.close();
-		
+
 		return fullPdfWritten;
 	}
-	
+
 	public void processUnitLine(){
 		//the idea is to absorb the characters starting with '|' or '||' and ending right before
 		//the next '|' or '||'s
@@ -138,20 +140,20 @@ public class PdfMaker {
 					charNum += dIndexes.get(curDIndex).freq;
 					xPos += dIndexes.get(curDIndex).freq * spacing;
 				}
-			
+
 				else if(line.charAt(charNum) == '<'){
 					String number = "";
 					charNum++;
 					for(; line.charAt(charNum) != '>'; charNum++) {
 						number += line.charAt(charNum);
 					}
-				
+
 					//need to advance charNum one more time to point right after the '>'
 					charNum++;
 					tUB.processInputWithinTriangle(line, number, xPos, yPos);
 					xPos += (number.length()+2)*spacing;
 				}	
-				
+
 				//if character is '-', then a line with width the value of 'spacing' would be drawn onto the
 				//pdf file
 				else if(line.charAt(charNum) == '-'){
@@ -159,48 +161,48 @@ public class PdfMaker {
 					charNum++;
 					xPos += spacing;
 				}
-				
+
 				//if the character is letter or digit it would be printed onto the pdf file 
 				else if(Character.isDigit(line.charAt(charNum))){
 					String number = "";
-				
+
 					for(; Character.isDigit(line.charAt(charNum)) ; charNum++) {
 						number += line.charAt(charNum);
 					}
-					
+
 					tUB.processDigit(number, lineNum, line, charNum, xPos, yPos, lastNumPos);
 					xPos += (number.length())*spacing;
 				}
-				
+
 				else if(line.charAt(charNum) == 's') {
 					tUB.slideUp(xPos, yPos);
 					charNum++;
 					xPos += spacing;
 				}
-			
+
 				else if(line.charAt(charNum) == '*') {
 					tUB.processRepeat(xPos, yPos);
 					xPos += spacing;
 					charNum++;
 				}
-			
+
 				else if(line.charAt(charNum) == ' ') {
 					charNum++;
 				}
-			
+
 				//write code for the cases that have not been handled
 				else if(line.charAt(charNum) == 'h'){
 					tUB.processH(xPos, yPos);
 					charNum++;
 					xPos += spacing;
 				}
-			
+
 				else if(line.charAt(charNum) == 'p'){
 					tUB.processP(xPos, yPos);
 					charNum++;
 					xPos += spacing;
 				}
-			
+
 				else if(line.charAt(charNum) == 'e' || line.charAt(charNum) == 'B' || line.charAt(charNum) == 'G' || line.charAt(charNum) == 'D'
 				|| line.charAt(charNum) == 'A' || line.charAt(charNum) == 'E'){
 					tUB.processChordID(line, charNum, xPos, yPos);
@@ -208,35 +210,35 @@ public class PdfMaker {
 					charNum++;
 				}
 			}
-			
-			
+
+
 			else{
 				charNum++;
 			}
 		}
 	}
-	
+
 	public void processMidBlockReInit(){
 		margin = xPos;
 		yPos = document.top() - yIncrement;
-		
+
 		//checking if the next block can be fit next to the current. If not, we need to print the
 		//trailing bars for the current block before the actual split that is coming up.
 		if(xPos + dIndexes.get(curDIndex).spaceNeeds > document.right()) {
-			
+
 			//if a block was split from the middle, the trailing '|'s should be printed before the split.
 			//processTrailingBars does that. Also the ending 6 horizontal lines are printed
 			xPos = tUB.processTrailingBars(curDIndex, contents.get(blockNum).size(), charNum, dIndexes.get(curDIndex).freq, dIndexes.get(curDIndex).repIndex, dIndexes.get(curDIndex).repNum, margin, xPos, yPos);
-			
+
 			yPos = document.top()-yIncrement;
-			
+
 			//if a block was split from the middle, the trailing '|'s should be printed before the split.
 			//and the same '|'s should be suppressed before the continuation on the next position, which is
 			//achieved through setting the skip variable.
 			skip = dIndexes.get(curDIndex).freq;
 		}
 	}
-	
+
 	public void processEndBlockReInit(){
 		margin = xPos;
 		yPos = document.top() - yIncrement;
@@ -244,25 +246,25 @@ public class PdfMaker {
 		//If all the units of a block have been printed, it is time to call 'processtrailingBars to print
 		//the last set of '|'s.
 		xPos = tUB.processTrailingBars(curDIndex, contents.get(blockNum).size(), charNum, dIndexes.get(curDIndex).freq, dIndexes.get(curDIndex).repIndex, dIndexes.get(curDIndex).repNum, margin, xPos, yPos);
-		
+
 		//margin and yPosition needs to be reset after processTrailingBars returns, so that the next
 		//line that would be output is done at the proper location. 
 		margin = xPos;
 		yPos = document.top() - yIncrement;
-		
+
 		blockNum++;
 		curDIndex = 0;
 	}
-	
+
 	public void processInsufficientSpaceReInit(){
 		//first draw the 6 horizontal lines at the end of the current block
 		tUB.processEndingHorizontalLines(xPos, yPos);
 		yPos = document.top()-yIncrement;
-		
+
 		margin = document.left();
 		xPos = margin;
 		yIncrement += fontSize * 8;
-		
+
 		if(yPos - fontSize * 8 - fontSize*6  < document.bottom()){
 			document.newPage();
 			yIncrement = 40f;
@@ -270,40 +272,40 @@ public class PdfMaker {
 			tUB.setLineWidth(0.5f);
 		}
 	}
-	
+
 	public ArrayList<DIndexFreq> getDIndexes(String source) {
 		ArrayList<DIndexFreq> dIndexes = new ArrayList<DIndexFreq>();
-		
+
 		for(int ind = 0; ind < source.length(); ind++) {
 			if(source.charAt(ind) == '|'){
-				
+
 				//'indexRep' represents the index of the repetition-value. An index of -1 is initially being assigned, which would be 
 				//replaced by the actual index value if a repetition-value is found. If no repetition value is found, a -1 would indicate that
 				//when a check would be done.
 				int indexRep = -1;
 				char repVal = 0;
-			
+
 				//We have come across the first '|' of the unit, so freq is 1 so far.
 				int freq = 1;
-				
+
 				//We need to keep track of the index of the first '|' in each DIndexFreq, which is assigned to temp because ind would increase
 				//in the next for loop.
 				int temp = ind;
 				ind++;
-				
+
 				//Calculating frequency of each occurrence of '|', as well as number of repeats
 				for(; ind < source.length() && source.charAt(ind) == '|'; ind++){
-					
+
 					//If there is a repeat, the repeat-number is in the first line.
 					if(Character.isDigit(contents.get(blockNum).get(0).charAt(ind))){
-						
+
 						//The index of the repeat-value is being stored.
 						indexRep = ind;
 						repVal = contents.get(blockNum).get(0).charAt(ind);
 					}
 					freq++;
 				}
-				
+
 				DIndexFreq data = new DIndexFreq();
 				data.index = temp;
 				data.freq = freq;
@@ -312,7 +314,7 @@ public class PdfMaker {
 				dIndexes.add(data);
 			}
 		}
-		
+
 		//If the first character is not '|', we have to explicitly tell the program where to start the block.
 		if(source.charAt(0) != '|'){
 			DIndexFreq data = new DIndexFreq();
@@ -322,7 +324,7 @@ public class PdfMaker {
 			data.repNum = 0;
 			dIndexes.add(0, data);
 		}
-		
+
 		//If the last index is not '|', we have to explicitly tell the program where to end the block.
 		if(source.charAt(source.length() - 1) != '|'){
 			DIndexFreq data = new DIndexFreq();
@@ -332,13 +334,13 @@ public class PdfMaker {
 			data.repNum = 0;
 			dIndexes.add(data);
 		}
-		
+
 		for(int ind = 0; ind < dIndexes.size()-1; ind++){
 			dIndexes.get(ind).spaceNeeds = ((dIndexes.get(ind + 1).index + dIndexes.get(ind + 1).freq)-(dIndexes.get(ind).index))*spacing;
 		}
 		return dIndexes;
 	}
-	
+
 	class DIndexFreq{
 		int index;
 		int freq;
@@ -347,4 +349,3 @@ public class PdfMaker {
 		float spaceNeeds;
 	}
 }
-
