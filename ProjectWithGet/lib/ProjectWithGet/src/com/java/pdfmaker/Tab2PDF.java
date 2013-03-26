34f289745e23f9d9daa911cc3a94c49539e8cbc2
macarr <matthew.carr96@gmail.com>
2013-03-26 11:19:15 -0400

    Tab2PDF uploaded. Not sure why it didn't come before

---
+++ ProjectWithGet/lib/ProjectWithGet/src/com/java/pdfmaker/Tab2PDF.java
@@ -0,0 +1,33 @@
+package com.java.pdfmaker;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.util.Date;
+
+public class Tab2PDF {
+	public static void main(String args[]){
+		String osVersion = System.getProperty("os.name");
+		String errorLog;
+		if(osVersion.startsWith("Windows"))
+			errorLog = ""+System.getenv("TEMP")+"/T2PDFErr.txt";
+		else
+			errorLog = "/tmp/T2PDFErr.txt";
+		File errorFile = new File(errorLog);
+		try {
+			if(!(errorFile.exists()))
+				errorFile.createNewFile();
+			PrintStream out = new PrintStream(new FileOutputStream(errorLog, true));
+			Date date = new Date();
+			out.println("["+date+"]");
+			System.setErr(out);
+		} catch(FileNotFoundException e) {
+			e.printStackTrace();
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
+		new TabUIMainWindow();
+	}
+}
