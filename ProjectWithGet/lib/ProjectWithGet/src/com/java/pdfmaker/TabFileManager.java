2365fef378ae1f252952aa0954efff1649ad4745
macarr <matthew.carr96@gmail.com>
2013-03-26 11:07:15 -0400

    bugfix. App actually runs now.

--- ProjectWithGet/lib/ProjectWithGet/src/com/java/pdfmaker/TabFileManager.java
+++ ProjectWithGet/lib/ProjectWithGet/src/com/java/pdfmaker/TabFileManager.java
@@ -43,7 +43,6 @@ public class TabFileManager{
 			outputPath = "" + System.getenv("TEMP");
 		else
 			outputPath = "/tmp/";
-		System.exit(0);
 	}
 
 	//'loadFile' displays the 'JFileChooser' which allows the user to choose the input file.  
