3b9671f26428f9ea4f9682c6c45dbb1cc9a3dad3
sayub <ayub.saad@ymail.com>
2013-03-31 14:22:45 -0400

    new packages

---
+++ ProjectWithGet/lib/ProjectWithGet/src/com/java/paramclasses/CharCoordinates.java
@@ -0,0 +1,17 @@
+package com.java.paramclasses;
+
+public class CharCoordinates{
+	public float xPos;
+	public float yPos;
+	public float rightMargin;
+	public float [] lastNumXPos;
+	public float [] lastNumYPos;
+	
+	public CharCoordinates(float xPos, float yPos, float rightMargin, float [] lastNumXPos, float [] lastNumYPos){
+		this.xPos = xPos;
+		this.yPos = yPos;
+		this.rightMargin = rightMargin;
+		this.lastNumXPos = lastNumXPos;
+		this.lastNumYPos = lastNumYPos;
+	}
+}
---
+++ ProjectWithGet/lib/ProjectWithGet/src/com/java/paramclasses/TextLine.java
@@ -0,0 +1,14 @@
+package com.java.paramclasses;
+
+public class TextLine {
+	public int lineNum;
+	public String line;
+	public int charNum;
+	
+	public TextLine(int lineNum, String line, int charNum){
+		this.lineNum = lineNum;
+		this.line = line;
+		this.charNum = charNum;
+	}
+}
+
