4eb39ac22001a52c33b328d90666b07320b0695d
sayub <ayub.saad@ymail.com>
2013-04-06 11:29:42 -0400

    fixed

---
+++ ProjectWithGet/lib/ProjectWithGet/src/com/java/paramclasses/ReturnValues.java
@@ -0,0 +1,11 @@
+package com.java.paramclasses;
+
+public class ReturnValues {
+	public float xPos;
+	public float yPos;
+	
+	public ReturnValues(float xPos, float yPos){
+		this.xPos = xPos;
+		this.yPos = yPos;
+	}
+}
---
+++ ProjectWithGet/lib/ProjectWithGet/src/com/java/paramclasses/TrailingCoordinates.java
@@ -0,0 +1,13 @@
+package com.java.paramclasses;
+
+public class TrailingCoordinates{
+	public float margin;
+	public float xPos;
+	public float yPos;
+	
+	public TrailingCoordinates(float margin, float xPos, float yPos){
+		this.margin = margin;
+		this.xPos = xPos;
+		this.yPos = yPos;
+	}
+}
